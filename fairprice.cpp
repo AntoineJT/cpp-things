fe830cac1d5f6c8c2507649b9c48dabd7de87392
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2020-06-17 04:06:48 +0200

    FairPrice: Make changes following gbdivers review
    P.S.: May use initialization with the {} (reread the arguments in their favor)

--- fairprice.cpp
+++ fairprice.cpp
@@ -2,12 +2,12 @@
 #include <iostream>
 #include <random>
 
-constexpr int MIN = 0;
-constexpr int MAX = 1000;
+constexpr int min = 0;
+constexpr int max = 1000;
 
 int random_int() {
     std::random_device rd;
-    std::uniform_int_distribution<int> dist(MIN, MAX);
+    std::uniform_int_distribution<int> dist(min, max);
     return dist(rd);
 }
 
@@ -17,14 +17,14 @@ int main() {
         << " Fair price game" << std::endl
         << "-----------------" << std::endl
         << std::endl
-        << "The number to guess is a random one under " << MIN << " and " << MAX << std::endl
+        << "The number to guess is a random one under " << min << " and " << max << std::endl
         << std::endl;
 
-    int number = random_int();
+    const int number = random_int();
     int round = 1;
-    int guess = 0;
 
     while (true) {
+        int guess;
         std::cout << "Your guess: ";
         std::cin >> guess;
 
@@ -36,8 +36,8 @@ int main() {
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         } else {
-            if (guess < MIN || guess > MAX) {
-                std::cout << "Number out of range (range: " << MIN << "-" << MAX << "). Try again." << std::endl;
+            if (guess < min || guess > max) {
+                std::cout << "Number out of range (range: " << min << "-" << max << "). Try again." << std::endl;
             } else {
                 if (guess < number) {
                     std::cout << "More." << std::endl;
